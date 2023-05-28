6033ff2398a7fa15818bd6651412a9913f16e444
andyphancode <andyphanpxh@gmail.com>
2023-05-28 10:43:44 -0700

    Added try/catch for error handling
    Added alternative routes of connecting in case of bugs

--- db.js
+++ db.js
@@ -5,19 +5,44 @@ const { getDatabaseUri } = require("./config");
 
 let db;
 
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-} else {
-  db = new Client({
-    connectionString: getDatabaseUri()
-  });
+
+
+
+try // windows route
+{
+  if (process.env.NODE_ENV === "production") {
+    db = new Client({
+      connectionString: getDatabaseUri(),
+      ssl: {
+        rejectUnauthorized: false
+      }
+    });
+  } else {
+    db = new Client({
+      connectionString: getDatabaseUri()
+    });
+  }
+  db.connect();
+
+  module.exports = db;
+} 
+catch // mac route
+{
+  if (process.env.NODE_ENV === "production") {
+    db = new Client({
+      // connectionString: `postgresql:///${getDatabaseUri()}`,
+      connectionString: getDatabaseUri(),
+      ssl: {
+        rejectUnauthorized: false
+      }
+    });
+  } else {
+    db = new Client({
+      // connectionString: `postgresql:///${getDatabaseUri()}`
+      connectionString: getDatabaseUri()
+    });
+  }
+  db.connect();
+
+  module.exports = db;
 }
-
-db.connect();
-
-module.exports = db;
