5c2237d78a77d6ffc456ff92b1657e5cf413bfc3
andyphancode <andyphanpxh@gmail.com>
2023-05-29 08:15:49 -0700

    added documentation
    added documentation for sqlForPartialUpdate function.

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,26 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+
+/**
+ * SQL helper function that creates an object we can use to help create the SET
+ * part of an update query. dataToUpdate contains the intended changes to the database,
+ * jsToSql ensures the correct column names are targeted in the query. Not every column
+ * has to be updated hence the partial update part of the function name.
+ * 
+ * @param {object} dataToUpdate - object with key:value pairs of new values to insert
+ * @param {object} jsToSql - maps the JS equivalent of a SQL column name to the SQL database
+ * column name. Example: {testColumn: "test_column"}
+ * @returns {object} {sqlSetCols, dataToUpdate}
+ * 
+ * Example: sqlForPartialUpdate(
+ * { test: "value1", testTwo: "v2"},
+ * { testTwo: "test_two"}) returns
+ * 
+ * {setCols: ""test"=$1, "test_two" =$2", values = ["v1", "v2"]}
+ * 
+ * Note that the original data to update passes keys "test" and "testTwo" but jsToSql changes "testTwo" to "test_two" in
+ * the final query
+ */ 
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
