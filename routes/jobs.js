242a20e926c7845223d93092650f3781144d43f5
andyphancode <andyphanpxh@gmail.com>
2023-05-30 10:31:51 -0700

    added post job route
    added post job route requiring admin privileges

---
+++ routes/jobs.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/** Routes for jobs. */
+
+const jsonschema = require("jsonschema");
+
+const express = require("express");
+const { BadRequestError } = require("../expressError");
+const { ensureAdmin } = require("../middleware/auth");
+const Job = require("../models/job");
+const jobNewSchema = require("../schemas/jobNew.json");
+const jobUpdateSchema = require("../schemas/jobUpdate.json");
+const jobSearchSchema = require("../schemas/jobSearch.json");
+
+const router = express.Router();
+
+
+/** POST / { job } =>  { job }
+ *
+ * job should be { title, salary, equity, companyHandle }
+ *
+ * Returns { id, title, salary, equity, companyHandle }
+ *
+ * Authorization required: admin
+ */
+
+router.post("/", ensureAdmin, async function (req, res, next) {
+    try {
+        const validator = jsonschema.validate(req.body, jobNewSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(error => error.stack);
+            throw new BadRequestError(errs);
+        }
+
+        const job = await Job.create(req.body);
+        return res.status(201).json({ job });
+    } catch (err) {
+        return next(err);
+    }
+});
