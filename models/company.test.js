7833dcd48870611ebac497246e0ff4e26a02b90d
andyphancode <andyphanpxh@gmail.com>
2023-06-02 14:10:14 -0700

    updated test to show job array

--- models/company.test.js
+++ models/company.test.js
@@ -211,6 +211,10 @@ describe("get", function () {
       description: "Desc1",
       numEmployees: 1,
       logoUrl: "http://c1.img",
+      jobs: [
+        {id: testJobIds[0], title: 'J1', salary: 1, equity: '0.1'},
+        {id: testJobIds[3], title: 'J4', salary: null, equity: null}
+      ]
     });
   });
 
